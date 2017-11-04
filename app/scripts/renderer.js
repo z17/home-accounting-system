67ef9c0b036652e7dec362135e4da26722aa12e8
YanLobat <yanlavryushev@gmail.com>
2017-11-04 21:24:53 +0300

    renderer update

--- app/scripts/renderer.js
+++ app/scripts/renderer.js
@@ -11,232 +11,198 @@ const incomeView = new IncomeView();
 const settingsView = new SettingsView();
 const languages = new Languages();
 
+let incomeEditingRow;
+let resizeTimeout;
+
 if (typeof google === 'undefined') {
-    alert(languages.getText('no-internet'));
-    throw new Error('no internet');
+  alert(languages.getText('no-internet'));
+  throw new Error('no internet');
 }
 
 google.charts.load("current", {packages: ['corechart']});
 
 ipcRenderer.on('error', function (event, data) {
-    alert(data);
+  alert(data);
 });
 
 ipcRenderer.on('income-data', function (event, data) {
-    incomeView.setData(data);
-    balanceView.setIncomeData(data);
+  incomeView.setData(data);
+  balanceView.setIncomeData(data);
 });
 
 ipcRenderer.on('income-payment-types', function (event, data) {
-    incomeView.setPaymentTypes(data);
+  incomeView.setPaymentTypes(data);
 });
 
 ipcRenderer.on('income-contacts', function (event, data) {
-    incomeView.setContacts(data);
+  incomeView.setContacts(data);
 });
 
 ipcRenderer.on('income-data-inserted', function (event, incomeItem) {
-    incomeView.insertIncome(incomeItem);
+  incomeView.insertIncome(incomeItem);
 });
 
 ipcRenderer.on('income-data-deleted', function (event, incomeId) {
-    incomeView.deleteIncome(incomeId);
+  incomeView.deleteIncome(incomeId);
 });
 
 ipcRenderer.on('income-edited', function (event, income) {
-    incomeView.updateIncome(income);
+  incomeView.updateIncome(income);
 });
 
 ipcRenderer.on('balance-inserted', function (event, source) {
-    balanceView.addBalanceSource(source);
+  balanceView.addBalanceSource(source);
 });
 
 ipcRenderer.on('balance-updated', function (event, id, month, sum) {
-    balanceView.addBalance(id, month, sum);
+  balanceView.addBalance(id, month, sum);
 });
 
 ipcRenderer.on('balance-reupdated', function (event, id, month) {
-    balanceView.deleteBalance(id, month);
+  balanceView.deleteBalance(id, month);
 });
 
 ipcRenderer.on('balance-types', function (event, types) {
-    balanceView.setBalance(types);
+  balanceView.setBalance(types);
 });
 
 ipcRenderer.on('settings-saved', function (event, data) {
-    settingsView.updateData(data);
+  settingsView.updateData(data);
 });
 
 ipcRenderer.on('settings', function (event, data) {
-    languages.setLanguage(data.language);
-    settingsView.setData(data);
-    ready('settings');
+  languages.setLanguage(data.language);
+  settingsView.setData(data);
+  init();
 });
 
-$(document).ready(function () {
-    ready('document');
+window.onclick = clicksHandler;
+
+window.onresize = resizeThrottler;
+
+$('a').click(onLinkClick);
+
+$('.js-tab').click(function () {
+  makeActive($(this).data('name'));
 });
 
-function ready(type) {
-    if (type == 'settings') {
-        this.settings = true;
-    }
+function init() {
+  document.documentElement.innerHTML = languages.replacePlaceholders(document.documentElement.innerHTML);
+  makeActive($('.js-tab.active'));
 
-    if (type == 'document') {
-        this.document = true;
-    }
+  incomeView.preparePage((incomeItem) => {
+    ipcRenderer.send('income-add', incomeItem);
+  });
 
-    if (this.settings && this.document) {
-        start();
+  balanceView.preparePage(
+    (source) => {
+      ipcRenderer.send('balance-add', source);
+    },
+    (id, month, value) => {
+      ipcRenderer.send('balance-update', id, month, value);
+    },
+    (id, month) => {
+      ipcRenderer.send('balance-month-remove', id, month);
     }
+  );
+
+  settingsView.preparePage((settings) => {
+    ipcRenderer.send('update-settings', settings);
+  });
 }
 
-function start() {
-    document.documentElement.innerHTML = languages.replacePlaceholders(document.documentElement.innerHTML);
+function clicksHandler(e) {
+  if (e.target.classList.contains('js-delete')) {
+    const id = e.target.dataset.id.toString();
+    return ipcRenderer.send('income-delete', id);
+  }
 
-    makeActive('income');
+  const row = e.target.closest('.js-income-row');
+  if ((row === null) || (row === incomeEditingRow)) {
+    return;
+  }
 
-    $('a').click(
-        function (e) {
-            if ($(this).attr('href') !== undefined) {
-                e.preventDefault();
-                shell.openExternal($(this).attr('href'));
-            }
-        }
-    );
+  if (incomeEditingRow !== undefined) {
+    const incomeItem = incomeView.getItemFromForm(incomeEditingRow);
+    ipcRenderer.send('income-edit', incomeItem);
+    incomeEditingRow = undefined;
+  }
 
-    let activeIncomeEditing = false;
-    let incomeEditingRow;
+  const copyField = ({ elementClass, fieldClass, options = {} }) => {
+    const { format } = options;
+    const element = row.querySelector(elementClass);
+    const field = document.querySelector(fieldClass).cloneNode(true);
+    let val  = element.innerHTML;
+    if (field.type == 'number') {
+      val = parseFloat(val.replace(' ', ''));
+    }
+    field.value = val;
+    if (format !== undefined) {
+      const time = element.dataset.time;
+      field.value = moment.unix(time).format(format);
+    }
+    element.innerHTML = '';
+    element.appendChild(field);
+  };
 
-    $(document).click(function (e) {
-        if (e.target.classList.contains('js-delete')) {
-            let id = e.target.dataset.id.toString();
-            ipcRenderer.send('income-delete', id);
-            return;
-        }
+  const fields = [{
+      elementClass: ".js-date",
+      fieldClass: ".js-add-date",
+      options: { format: "YYYY-MM-DD" }
+    },
+    {
+      elementClass: ".js-month",
+      fieldClass: ".js-add-month",
+      options: { format: "YYYY-MM" }
+    },
+    { elementClass: ".js-sum", fieldClass: ".js-add-sum" },
+    { elementClass: ".js-payment-type", fieldClass: ".js-add-payment-type" },
+    { elementClass: ".js-contact", fieldClass: ".js-add-contact" },
+    { elementClass: ".js-description", fieldClass: ".js-add-description" }];
 
-        let updateIncome = () => {
-            if (!activeIncomeEditing) {
-                return;
-            }
+  fields.forEach(copyField);
+  incomeEditingRow = row;
+}
 
-            const incomeItem = incomeView.getItemFromForm(incomeEditingRow);
-            ipcRenderer.send('income-edit', incomeItem);
-
-            activeIncomeEditing = false;
-        };
-
-        if ($(e.target).closest('.js-income-row').length !== 0) {
-            let row = e.target.closest('.js-income-row');
-
-            if (activeIncomeEditing === true && row !== incomeEditingRow) {
-                updateIncome();
-            }
-
-            if (activeIncomeEditing === true) {
-                return;
-            }
-
-
-            let copyField = (elementClass, fieldClass) => {
-                let element = row.querySelector(elementClass);
-                let field = document.querySelector(fieldClass).cloneNode(true);
-                let val = element.innerHTML;
-                if (field.type == 'number') {
-                    val = parseFloat(val.replace(' ', ''));
-                }
-                field.value = val;
-                element.innerHTML = '';
-                element.appendChild(field);
-            };
-
-            let copyDateField = (elementClass, fieldClass, format) => {
-                let element = row.querySelector(elementClass);
-                let field = document.querySelector(fieldClass).cloneNode(true);
-                let time = element.dataset.time;
-                field.value = moment.unix(time).format(format);
-                element.innerHTML = '';
-                element.appendChild(field);
-            };
-
-            copyDateField('.js-date', '.js-add-date', "YYYY-MM-DD");
-            copyDateField('.js-month', '.js-add-month', "YYYY-MM");
-            copyField('.js-sum', '.js-add-sum');
-            copyField('.js-payment-type', '.js-add-payment-type');
-            copyField('.js-contact', '.js-add-contact');
-            copyField('.js-description', '.js-add-description');
-
-            activeIncomeEditing = true;
-            incomeEditingRow = row;
-            return;
-        }
-
-        updateIncome();
-
-    });
-
-    $('.js-tab').click(function () {
-        makeActive($(this).data('name'));
-    });
-
-    incomeView.preparePage((incomeItem) => {
-        ipcRenderer.send('income-add', incomeItem);
-    });
-
-    balanceView.preparePage(
-        (source) => {
-            ipcRenderer.send('balance-add', source);
-        },
-        (id, month, value) => {
-            ipcRenderer.send('balance-update', id, month, value);
-        },
-        (id, month) => {
-            ipcRenderer.send('balance-month-remove', id, month);
-        }
-    );
-
-    settingsView.preparePage((settings) => {
-        ipcRenderer.send('update-settings', settings);
-    });
+function onLinkClick(e) {
+  if ($(this).attr('href') !== undefined) {
+    e.preventDefault();
+    shell.openExternal($(this).attr('href'));
+  }
 }
 
 function makeActive(tabName) {
-    $('.js-page').removeClass('active');
-    $('.js-tab').removeClass('active');
-    $('.js-tab[data-name=' + tabName + ']').addClass('active');
-    $('.js-page[data-name="' + tabName + '"]').addClass('active');
-    reloadGraph(tabName);
+  const name = tab.data('name');
+  $('.js-page').removeClass('active');
+  $('.js-tab').removeClass('active');
+  $('.js-tab[data-name=' + tabName + ']').addClass('active');
+  $('.js-page[data-name="' + tabName + '"]').addClass('active');
+  reloadGraph(tabName);
 }
 
 function reloadGraph(tabName) {
-    switch (tabName) {
-        case 'income':
-            incomeView.reloadGraph();
-            break;
-        case 'balance':
-            balanceView.reloadGraph();
-            break;
-        default:
-            alert('Unknown tab name: ' + name);
-    }
+  switch (tabName) {
+    case 'income':
+      incomeView.reloadGraph();
+      break;
+    case 'balance':
+      balanceView.reloadGraph();
+      break;
+    default:
+      alert('Unknown tab name: ' + name);
+  }
 }
 
-(function () {
-    window.addEventListener("resize", resizeThrottler, false);
+function resizeThrottler() {
+  if (!resizeTimeout) {
+    resizeTimeout = setTimeout(function () {
+      resizeTimeout = null;
+      actualResizeHandler();
+    }, 250);
+  }
+}
 
-    let resizeTimeout;
-
-    function resizeThrottler() {
-        if (!resizeTimeout) {
-            resizeTimeout = setTimeout(function () {
-                resizeTimeout = null;
-                actualResizeHandler();
-            }, 250);
-        }
-    }
-
-    function actualResizeHandler() {
-        let name = $('.js-tab.active').data('name');
-        reloadGraph(name);
-    }
-}());
+function actualResizeHandler() {
+  let name = $('.js-tab.active').data('name');
+  reloadGraph(name);
+}
